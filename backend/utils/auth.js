6eaf0cfd5cd5df482b08fea5914eee8c31e46ff6
jane martin <martin.jane.ellis@gmail.com>
2021-04-22 13:37:07 -0400

    Complete addition of user auth middleware

--- backend/utils/auth.js
+++ backend/utils/auth.js
@@ -4,6 +4,8 @@ const { User } = require('../db/models');
 
 const { secret, expiresIn } = jwtConfig;
 
+/* Set up the identity of the current user upon login or signup */
+
 const setTokenCookie = (res, user) => {
     const token = jwt.sign(
         { data: user.toSafeObject() },
@@ -22,3 +24,45 @@ const setTokenCookie = (res, user) => {
 
     return token;
 };
+
+/* Grab the identity of the current user - especially for user to access
+authenticated routes */
+
+const restoreUser = (req, res, next) => {
+    const { token } = req.cookies;
+
+    return jwt.verify(token, secret, null, async (err, jwtPayload) => {
+        if (err) {
+            return next();
+        }
+
+        try {
+            const { id } = jwtPayload.data;
+            req.user = await User.scope('currentUser').findByPk(id);
+        } catch (e) {
+            res.clearCookie('token');
+            return next();
+        }
+
+        if (!req.user) res.clearCookie('token');
+
+        return next();
+    });
+};
+
+/* Use the identity of the current user to authenticate them before they 
+access certain routes */
+const requireAuth = [
+    restoreUser,
+    function (req, res, next) {
+        if (req.user) return next();
+
+        const err = new Error('Unauthorized');
+        err.title = 'Unauthorized';
+        err.errors = ['Unauthorized'];
+        err.status = 401;
+        return next(err);
+    },
+];
+
+module.exports = { setTokenCookie, restoreUser, requireAuth };
