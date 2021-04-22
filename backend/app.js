f96d2ba4911874928aee835dcbb379b66f8e9299
jane martin <martin.jane.ellis@gmail.com>
2021-04-22 12:43:27 -0400

    Add Sequelize error handling

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,8 @@ const cors = require('cors');
 const csurf = require('csurf');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
+const { ValidationError } = require('sequelize');
+
 const { environment } = require('./config');
 const routes = require('./routes');
 
@@ -39,6 +41,14 @@ app.use((_req, _res, next) => {
     err.title = "Resource Not Found";
     err.errors = ["The requested resource couldn't be found."];
     err.status = 404;
+    next(err);
+});
+
+app.use((err, _req, _res, next) => {
+    if (err instanceof ValidationError) {
+        err.errors = err.errors.map((e) => e.message);
+        err.title = 'Validation error';
+    }
     next(err);
 });
 
