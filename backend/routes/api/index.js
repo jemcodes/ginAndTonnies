276d59ad1f9ad9f318db02463638735efa998724
jane martin <martin.jane.ellis@gmail.com>
2021-04-22 14:15:37 -0400

    Test user login route - up and running

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,9 +1,15 @@
-const router = require('express').Router();
 const asyncHandler = require('express-async-handler');
+
+const router = require('express').Router();
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
 const { setTokenCookie } = require('../../utils/auth.js');
 const { User } = require('../../db/models');
 const { restoreUser } = require('../../utils/auth.js');
 const { requireAuth } = require('../../utils/auth.js');
+
+router.use('/session', sessionRouter);
+router.use('/users', usersRouter);
 
 router.post('/test', function (req, res) {
     res.json({ requestBody: req.body });
