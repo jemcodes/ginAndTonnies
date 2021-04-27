117f169aa59b5f2435111a18f75de543636e1dc0
jane martin <martin.jane.ellis@gmail.com>
2021-04-27 00:14:01 -0400

    Add backend route to access a single review

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -3,6 +3,7 @@ const express = require('express');
 const router = express.Router({ mergeParams: true });
 const db = require('../../db/models');
 
+// GET a list of all the reviews
 router.get('/', asyncHandler(async (req, res, next) => {
     const drinkId = parseInt(req.params.id, 10);
     const reviewsToShow = await db.Review.findAll( {
@@ -14,5 +15,17 @@ router.get('/', asyncHandler(async (req, res, next) => {
     // console.log(reviewsToShow)
     return res.json( reviewsToShow )
 }))
+
+// GET a single review
+router.get('/:id(\\d+)', asyncHandler(async (req, res) => {
+    const reviewId = parseInt(req.params.id, 10);
+    const singleReview = await db.Review.findByPk(reviewId, {
+        include: [db.User]
+    });
+    return res.json(singleReview);
+}))
+
+// POST to create a new review
+router.post('/', asyncHandler(async (req, res)))
 
 module.exports = router;
