61047f0819e69fc8924ef9cfcd3611b06936bb25
jane martin <martin.jane.ellis@gmail.com>
2021-04-28 15:36:25 -0400

    Begin CSS work from design mock-ups

---
+++ frontend/src/components/SplashPage/index.js
@@ -0,0 +1,11 @@
+import './SplashPage.css'
+
+function SplashPage() {
+    return (
+        <div class="splash-page-container">
+         <div class="splash-page-masthead">
+         </div>   
+        </div>
+    )
+}
+export default SplashPage;
