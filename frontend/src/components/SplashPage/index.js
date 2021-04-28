e719727741b0a74d1e66c16c76e09a6b1aa84193
jane martin <martin.jane.ellis@gmail.com>
2021-04-28 16:50:54 -0400

    Add preliminary grid to test out style

--- frontend/src/components/SplashPage/index.js
+++ frontend/src/components/SplashPage/index.js
@@ -2,9 +2,26 @@ import './SplashPage.css'
 
 function SplashPage() {
     return (
-        <div class="splash-page-container">
-         <div class="splash-page-masthead">
-         </div>   
+        <div className="splash-wrapper">
+            <div className="splash-hero"><h3>Welcome to gin & tonnies</h3>
+            </div>
+            <div className="splash-demo-panel">
+                <h1>
+                    Welcome To gin & tonnies
+                </h1>
+                <h3>Drink Delightfully</h3>
+            </div>
+            <div className="section">
+                <div className="cards">
+                    <h2>Ratings & Reviews</h2>
+                    <button>See More</button>
+                </div>
+                <div className="cards">
+                    <h2>Badges</h2>
+                    <button>See More</button>
+                </div>
+            </div>
+            <div className="footer"><h3>Contact Me</h3></div>
         </div>
     )
 }
